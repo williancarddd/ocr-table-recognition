96a6ba926d0a862c624eb64d44bb98ac6e3db96c
Lucas Zampar <lucas.26.zampar@gmail.com>
2025-11-27 20:35:29 -0300

    Adição da classe estática ICDARYoloConverter para o reaproveitamento de métodos chaves em outros códigos com aproveitamento de parte do código do João Victor

--- DataExtractor/yolo_converter.py
+++ DataExtractor/yolo_converter.py
@@ -1,9 +1,133 @@
 import os
 import shutil
-import random
 import xml.etree.ElementTree as ET
 from PIL import Image
 from glob import glob
+
+import pandas as pd
+from pathlib import Path
+from typing import Iterable, Tuple
+
+import random
+
+
+class ICDARYOLOConverter:
+    # Deixei a classe como estática 
+    class_id = 0
+    class_label = 'cell'
+
+    @staticmethod
+    def create_folders(root_dataset_path : str | Path, 
+                       include_test : bool = False):
+
+        split_paths = ['train', 'val']
+        if include_test:
+            split_paths+= ['test']
+
+        if not os.path.exists(root_dataset_path):
+            os.makedirs(root_dataset_path)
+
+        for split_path in split_paths:  
+            os.makedirs(os.path.join(root_dataset_path, split_path, 'images'), exist_ok=True)
+            os.makedirs(os.path.join(root_dataset_path, split_path, 'labels'), exist_ok=True)
+    
+    @staticmethod
+    def normalize_mask_points( 
+                         mask : Iterable[Tuple[float]], 
+                         image_width : float, 
+                         image_height : float):
+        '''
+        As coordenadas dos pontos dos polígonos devem ser normalizados pela respectiva 
+        dimensão da imagem. Dessa forma, x_norm = x / image_width e y_norm = y / image_height.
+        No arquivo TXT final, deve constar class_id x1_norm y2_norm x2_norm y2_norm ... xn_norm yn_norm 
+        '''
+
+        new_xy_points = []
+        for xy_point in mask:
+            x, y = xy_point
+
+            x_norm = x / image_width
+            y_norm = y / image_height
+            
+            # lista para facilitar a conversão pelo JSON ao ler a partir do CSV
+            new_xy_points.append([x_norm, y_norm])
+        
+        return new_xy_points
+    
+    @staticmethod
+    def normalize_masks(
+            masks : Iterable[Iterable[Tuple[float]]],
+            image_width : float, 
+            image_height : float):
+        
+        '''
+        Normaliza todas as máscaras contidas em uma iterável de máscaras.
+        '''
+
+        normalized_masks = []
+    
+        for mask in masks:        
+            # normaliza os pontos da máscara
+            normilized_mask = ICDARYOLOConverter.normalize_mask_points(mask, image_width, image_height)  
+            normalized_masks.append(normilized_mask) 
+        
+        return normalized_masks
+
+    @staticmethod
+    def create_txt_file_content(normalized_masks : Iterable[Iterable[Tuple[float]]]):
+
+        lines = []
+        for mask in normalized_masks:
+            # constroi a string com os pontos normalizados x1_norm y2_norm x2_norm y2_norm ... xn_norm yn_norm 
+            poly_str = " ".join([f"{coord:.6f}" for xy_point in mask for coord in xy_point ])
+            # define a string que representa a linha com class_id x1_norm y2_norm x2_norm y2_norm ... xn_norm yn_norm 
+            line = f"{ICDARYOLOConverter.class_id} {poly_str}"
+            # adiciona a linnha na lista de linhas
+            lines.append(line)       
+
+        # concatena todas as linhas do arquivo TXT em uma string separadas pela quebra de linha
+        return "\n".join(lines)
+
+    @staticmethod
+    def create_txt_file(content : str,  path : str | Path):
+        with open(path, 'w') as txt_file: 
+            txt_file.write(content)
+
+
+    def process_masks(masks : Iterable[Iterable[Tuple[float]]], 
+                      image_width : int, 
+                      image_height : int,
+                      path_txt_file : str | Path):
+        
+        normalized_masks = ICDARYOLOConverter.normalize_masks(masks, image_width, image_height)
+        txt_file_content =  ICDARYOLOConverter.create_txt_file_content(normalized_masks)
+        ICDARYOLOConverter.create_txt_file(txt_file_content, path_txt_file)
+
+        return normalized_masks
+
+    
+    @staticmethod
+    def create_yaml( 
+                    output_dir : str | Path = None, 
+                    train_fold_path : str | Path = None, 
+                    val_fold_path : str | Path = None):  #cria um yaml
+
+        yaml_path = os.path.join(output_dir, "dataset.yaml")
+        
+        with open(yaml_path, "w") as f:
+            f.write(
+f"""path: {os.path.abspath(output_dir)}
+train: {os.path.abspath(train_fold_path)}
+val: {os.path.abspath(val_fold_path)}
+task: segment
+
+names:
+  {ICDARYOLOConverter.class_id}: {ICDARYOLOConverter.class_label}
+"""
+            )
+        
+        print(f"\nYAML criado em: {yaml_path}")
+        return yaml_path
 
 
 class YOLOConverter:
